4442878ec96732c208cc0b668b7b3a296626c07e
mzbali <mahmud.bali@tuta.io>
2021-06-08 22:41:56 +0600

    Added dynamic list of expenseItem

--- starting-setup/src/components/Expenses/Expenses.js
+++ starting-setup/src/components/Expenses/Expenses.js
@@ -19,20 +19,17 @@ const Expenses = (props) => {
           selectYear={filteredYear}
           onYearChange={yearChangeHandler}
         />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            price={expense.amount}
+            date={expense.date}
+          />
+        ))}
         <ExpenseItem
-          title={props.items[0].title}
-          price={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          price={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          price={props.items[2].amount}
-          date={props.items[2].date}
+          title={props.items[3].title}
+          price={props.items[3].amount}
+          date={props.items[3].date}
         />
       </Card>
     </div>
