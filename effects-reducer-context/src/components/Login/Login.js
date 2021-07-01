d4a18b1f3e06248baf60130d4006f14d0075c1c8
mzbali <mahmud.bali@tuta.io>
2021-07-02 01:08:40 +0600

    removed some commented code

--- effects-reducer-context/src/components/Login/Login.js
+++ effects-reducer-context/src/components/Login/Login.js
@@ -16,19 +16,16 @@ const emailReducer = (state, action) => {
 };
 
 const passwordReducer = (state, action) => {
-  if (action.type === 'PASSWORD_INPUT') {
+  if (action.type === 'USER_INPUT') {
     return { value: action.value, isValid: action.value.trim().length > 6 };
   }
-  if (action.type === 'PASSWORD_BLUR') {
+  if (action.type === 'USER_BLUR') {
     return { value: state.value, isValid: state.value.trim().length > 6 };
   }
   return { value: '', isValid: null };
 };
 
 const Login = (props) => {
-  // const [enteredEmail, setEnteredEmail] = useState('');
-  // const [emailIsValid, setEmailIsValid] = useState();
-
   // const [emailState, setEmailState] = useState({ value: '', isValid: null }); //v2
 
   const [emailState, dispatchEmail] = useReducer(emailReducer, {
@@ -36,33 +33,11 @@ const Login = (props) => {
     isValid: null,
   });
 
-  // const [enteredPassword, setEnteredPassword] = useState('');
-  // const [passwordIsValid, setPasswordIsValid] = useState();
-
   const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
     value: '',
     isValid: null,
   });
   const [formIsValid, setFormIsValid] = useState(false);
-
-  // useEffect(() => {
-  //   console.log('Effect Running');
-
-  //   return () => {
-  //     console.log('Cleanup Running');
-  //   };
-  // }, [enteredPassword]);
-
-  // useEffect(() => {
-  //   const identifier = setTimeout(() => {
-  //     setFormIsValid(
-  //       enteredEmail.includes('@') && enteredPassword.trim().length > 6
-  //     );
-  //   }, 500);
-  //   return () => {
-  //     clearTimeout(identifier); //(some trigger changing state or prop this effects dependency)=>>>> previous component unmount, this runs, next state mount, effect runs.
-  //   };
-  // }, [enteredEmail, enteredPassword]);
 
   const { isValid: emailIsValid } = emailState;
   const { isValid: passwordIsValid } = passwordState;
@@ -79,46 +54,21 @@ const Login = (props) => {
   }, [emailIsValid, passwordIsValid]);
 
   const emailChangeHandler = (event) => {
-    // setEnteredEmail(event.target.value); //on every change render after two iteration
-
-    // setEmailState((prevEmailState) => { //v2
-    //   //useState object
-    //   return {
-    //     ...prevEmailState,
-    //     value: event.target.value, // save character on change
-    //   };
-    // });
-
     dispatchEmail({ type: 'USER_INPUT', value: event.target.value });
-
-    // setFormIsValid(event.target.value.includes('@') && passwordState.isValid); //maybe the code ran too early
   };
 
   const passwordChangeHandler = (event) => {
-    // setEnteredPassword(event.target.value);
-
-    dispatchPassword({ type: 'PASSWORD_INPUT', value: event.target.value }); //could be 6
+    dispatchPassword({ type: 'USER_INPUT', value: event.target.value }); //could be 6
 
     // setFormIsValid(emailState.value.includes('@') && event.target.isValid); //maybe the code ran too early //dispatch(state) is scheduled, so still 5
   };
 
   const validateEmailHandler = () => {
-    // setEmailIsValid(enteredEmail.includes('@')); //enteredEmail could be an older snapshot when, unblurred trriggerd.
-
-    // setEmailState((prevEmailState) => { //v2
-    //   //useState object
-    //   return {
-    //     ...prevEmailState,
-    //     isValid: prevEmailState.value.includes('@'), //check and save validity on unblur
-    //   };
-    // });
-
     dispatchEmail({ type: 'INPUT_BLUR' });
   };
 
   const validatePasswordHandler = () => {
-    dispatchPassword({ type: 'PASSWORD_BLUR' });
-    // setPasswordIsValid(enteredPassword.trim().length > 6);
+    dispatchPassword({ type: 'USER_BLUR' });
   };
 
   const submitHandler = (event) => {
